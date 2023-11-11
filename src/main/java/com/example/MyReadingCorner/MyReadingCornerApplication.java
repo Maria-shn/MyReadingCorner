78630553eb76265f0d533b29449604a854bf14c6
Maria <maria.shn.es@gmail.com>
2023-11-11 21:16:59 +0100

    Initial commit

---
+++ src/main/java/com/example/MyReadingCorner/MyReadingCornerApplication.java
@@ -0,0 +1,13 @@
+package com.example.MyReadingCorner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyReadingCornerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyReadingCornerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MyReadingCorner/MyReadingCornerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyReadingCorner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyReadingCornerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
